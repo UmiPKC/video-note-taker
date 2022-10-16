3c22db839db3f19bb75a70c253868904f1706507
Umi <umisft@gmail.com>
2022-10-15 23:02:16 -0400

    Beginning to develop frontend HTML and CSS more. Added more proper looking navbar

--- note_taker/models.py
+++ note_taker/models.py
@@ -10,6 +10,9 @@ class Notebook(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE) 
 	youtube_id = models.CharField(max_length=100, default="Will fill automatically") #youtube ID #default is furret vid
 	notebook_id = models.CharField(max_length=100, default='Randomly generated') #randomly generated id
+	description = models.CharField(max_length=500, default=None, blank=True, null=True)
+	#game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+	privacy = models.CharField(max_length=100, default="Private") #Private or Public
 
 	def __str__(self):
 		return "%s, Youtube ID: %s, Notebook ID: %s" % (self.title, self.youtube_id, self.notebook_id)
@@ -19,6 +22,8 @@ class Note(models.Model):
 	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now)
 	notebook = models.ForeignKey(Notebook, on_delete=models.CASCADE)
+	likes = models.IntegerField(default=0)
+
 
 	def __str__(self):
 		show = "%s - %s" % (self.notebook, self.timestamp)
